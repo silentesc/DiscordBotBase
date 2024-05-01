cea3e93938d9b5c33206d950b5d0ce3a1f0eb4a7
silentesc <scope-frisk-copy@duck.com>
2024-05-01 14:53:14 +0200

    Logging unexpected errors

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Client, GatewayIntentBits, ActivityType } from "discord.js";
+import { Client, ActivityType, EmbedBuilder } from "discord.js";
 import { readdirSync } from "fs";
 import "dotenv/config";
 
@@ -10,7 +10,7 @@ readdirSync("./src/commands").filter(file => file.endsWith(".ts")).forEach(fileN
     commands.set(commandName, command);
 });
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [] });
 
 
 client.on("ready", c => {
@@ -27,7 +27,21 @@ client.on("interactionCreate", async interaction => {
 
     const command = commands.get(interaction.commandName);
     if (command) {
-        await command.execute(client, interaction);
+        try {
+            await command.execute(client, interaction);
+        }
+        catch (error: any) {
+            const timestamp = new Date().toISOString().replace('T', ' ').replace(/\..+/, '');
+            console.log(
+                `[${timestamp}] Catched error\nExecuter: ${interaction.user.tag}\nCommand: ${interaction.commandName}\nOptions: ${interaction.options.data}\n${error.stack}\nEnd of error`
+            );
+
+            const responseEmbed = new EmbedBuilder()
+                .setColor(0xfa4b4b)
+                .setTitle("❗Error❗")
+                .setDescription("An unexpected error occured while executing that command.\n**Please contact an admin or dev so it can be fixed!**");
+            await interaction.reply({ embeds: [responseEmbed], ephemeral: true });
+        }
     }
 });
 
